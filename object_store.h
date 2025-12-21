02f50da81a514e9ce86f1ac488b0f44dd427fc05
M Samarth Mahendra <mahendra.ma@northeastern.edu>
2025-12-21 13:17:30 -0500

    added object store header with map and mutex

---
+++ object_store.h
@@ -0,0 +1,31 @@
+//
+// Created by Samarth Mahendra on 12/21/25.
+//
+
+#ifndef OBJECT_STORE_H
+#define OBJECT_STORE_H
+#include <mutex>
+#include <unordered_map>
+
+#include "object_ref.h"
+
+
+namespace orion {
+
+    // Simple in-memory object store.
+    // Maps ObjectId -> std::any
+    class ObjectStore {
+    public:
+        void put(const ObjectId& id, std::any value);
+        std::optional<std::any> get(const ObjectId& id);
+    private:
+        std::unordered_map<ObjectId, std::any> store_;
+        std::mutex mutex_;
+    };
+
+
+}
+
+
+
+#endif //OBJECT_STORE_H
